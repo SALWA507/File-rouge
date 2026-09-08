ac307912e6d3fdce612eed72abc72eeb4b07a385
SALWA507 <salwaamzane30@gmail.com>
2026-09-08 10:38:27 +0100

    ajouter function register dans AuthController

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -8,6 +8,28 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+{
+    $data = $request->validate([
+        'name' => ['required', 'string', 'max:255'],
+        'email' => ['required', 'email', 'max:255', 'unique:users,email'],
+        'password' => ['required', 'string', 'min:8', 'confirmed'],
+    ]);
+
+    $user = User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'password' => Hash::make($data['password']),
+    ]);
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json([
+        'message' => 'Compte créé avec succès',
+        'token' => $token,
+        'user' => $user
+    ], 201);
+}
     public function login(Request $request)
     {
         $credentials = $request->validate([
