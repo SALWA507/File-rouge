a82e7dc9c8574bff2446c7ef5b899865403ed764
SALWA507 <salwaamzane30@gmail.com>
2026-09-09 12:01:51 +0100

    définir les champs, cast et relations du modèle Alert

--- backend/app/Models/Alert.php
+++ backend/app/Models/Alert.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Alert extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'equipment_id',
+        'message',
+        'type',
+        'isRead',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'isRead' => 'boolean',
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function equipment()
+    {
+        return $this->belongsTo(Equipment::class);
+    }
 }
