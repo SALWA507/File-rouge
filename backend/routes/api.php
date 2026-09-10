5fe8a0f0994627faea758836b6dc627d9891e936
SALWA507 <salwaamzane30@gmail.com>
2026-09-10 10:45:17 +0100

    ajouter route de alert

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EquipmentController;
 use App\Http\Controllers\MaintenanceController;
 use App\Http\Controllers\InterventionController;
+use App\Http\Controllers\AlertController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
@@ -15,4 +16,5 @@ Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::apiResource('equipment', EquipmentController::class);
 Route::apiResource('maintenances', MaintenanceController::class);
 Route::apiResource('interventions', InterventionController::class);
+Route::apiResource('alerts', AlertController::class);
 });
