73d02d025504b8c0ed0cb5e77fea684b59b12ecf
SALWA507 <salwaamzane30@gmail.com>
2026-09-10 10:03:35 +0100

    ajouter api des maintenances

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\MaintenanceController;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
@@ -11,4 +12,5 @@ Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/user', [AuthController::class, 'user']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::apiResource('equipment', EquipmentController::class);
+Route::apiResource('maintenances', MaintenanceController::class);
 });
